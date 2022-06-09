fd1c3f629adb45725b4b3b5c4cba56a862befb0f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-10 00:27:12 +0300

    feat - add api versioning and auth folder

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware'=>'api', 'prefix'=>'auth'], function($router) {
+// add api versioning
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['middleware'=>'api', 'prefix'=>'auth'], function($router) {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('refresh', [AuthController::class, 'refresh']); // refresh a token
     Route::get('profile', [AuthController::class, 'profile']);
-    Route::get('logout', [AuthController::class, 'logout']);
+    });
 });
