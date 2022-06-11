aef621993cbee02608072227d13d8a10f9e7a9e9
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-11 13:03:32 +0300

    feat - add category, favorite models

---
+++ backend/database/migrations/2022_06_11_100143_create_favorites_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->foreignId(column: 'user_id')->constrained(table: 'users');
+            $table->foreignId(column: 'product_id')->constrained(table: 'products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
