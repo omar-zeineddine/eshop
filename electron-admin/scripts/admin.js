42d8cbf3a2b2bd2f822aafca83a8ca4015e6e0fd
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-13 05:57:47 +0300

    feat - add admin auth routes and categories functions

--- electron-admin/scripts/admin.js
+++ electron-admin/scripts/admin.js
@@ -7,7 +7,7 @@ navToggle.addEventListener("click", () => {
 });
 
 // check token
-let token = localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
 if (!token) {
   location.href = "index.html";
@@ -32,3 +32,48 @@ logout_btn.addEventListener("click", async function () {
 
   location.reload();
 });
+
+const add_cat_btn = document.getElementById("add_category_btn");
+let category_name = document.getElementById("category_name");
+
+add_cat_btn.addEventListener("click", async function () {
+  if (category_name.value == "") {
+    alert("enter a category name");
+  } else {
+    let category_data = new FormData();
+    category_data.append("name", category_name.value);
+
+    await axios({
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+      },
+      method: "post",
+      url: "http://127.0.0.1:8000/api/v1/admin/add_category",
+      data: category_data,
+    }).then(function (res) {
+      if (res.data["success"]) {
+        console.log(res);
+      } else {
+        alert("Something went wrong");
+      }
+    });
+  }
+});
+
+const getAllCategories = async () => {
+  const config = {
+    header: { Accept: "application/json", Authorization: `Bearer ${token}` },
+  };
+  try {
+    const res = await axios.get(
+      `http://127.0.0.1:8000/api/v1/admin/all_categories`,
+      config
+    );
+    console.log(res.data);
+  } catch (e) {
+    console.log("Error ", e);
+  }
+};
+
+getAllCategories();
