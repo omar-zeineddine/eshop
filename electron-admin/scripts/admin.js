d1128d6354c8010dc24bec1e7f62c370b8ee9abe
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-13 10:25:38 +0300

    feat - add new product for admin panel

--- electron-admin/scripts/admin.js
+++ electron-admin/scripts/admin.js
@@ -35,6 +35,32 @@ logout_btn.addEventListener("click", async function () {
 
 const add_cat_btn = document.getElementById("add_category_btn");
 let category_name = document.getElementById("category_name");
+const add_btn = document.getElementById("add_btn");
+
+add_btn.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const product_name = document.getElementById("product_name");
+  const product_price = document.getElementById("product_price");
+  const product_stock = document.getElementById("product_stock");
+  const product_category = document.getElementById("product_category");
+
+  let reader = new FileReader();
+  reader.addEventListener("loadend", function () {
+    let data = new FormData();
+    data.append("name", product_name.value);
+    data.append("price", product_price.value);
+    data.append("stock", product_stock.value);
+    data.append("category_id", product_category.value);
+    axios({
+      method: "post",
+      url: "http//127.0.0.1:8000/api/v1/admin/add_product",
+      headers: { Authorization: `Bearer ${token}` },
+      data: data,
+    }).then(function (res) {
+      window.location.reload();
+    });
+  });
+});
 
 add_cat_btn.addEventListener("click", async function () {
   if (category_name.value == "") {
